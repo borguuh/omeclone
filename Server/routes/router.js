e07041efdf1c2ce4f0588daa1794b6e0787fe4af
Borguuh <abdrahmanadewale@gmail.com>
2023-03-09 07:22:07 +0100

    build webRTC connection

--- Server/routes/router.js
+++ Server/routes/router.js
@@ -5,4 +5,7 @@ const services = require("../services/render");
 
 route.get("/", services.homeRoutes);
 
+route.get("/video_chat", services.video_chat);
+route.get("/text_chat", services.text_chat);
+
 module.exports = route;
--- server.js
+++ server.js
@@ -19,3 +19,70 @@ app.use("/", require("./Server/routes/router"));
 var server = app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
+
+const io = require("socket.io")(server, {
+  allowEIO3: true, //False by default
+});
+
+var userConnection = [];
+
+io.on("connection", (socket) => {
+  console.log("Socket id is: ", socket.id);
+
+  socket.on("userconnect", (data) => {
+    console.log("Logged in username", data.displayName);
+    userConnection.push({
+      connectionId: socket.id,
+      user_id: data.displayName,
+    });
+
+    var userCount = userConnection.length;
+    console.log("UserCount", userCount);
+  });
+  socket.on("offerSentToRemote", (data) => {
+    var offerReceiver = userConnection.find(
+      (o) => o.user_id === data.remoteUser
+    );
+    if (offerReceiver) {
+      console.log("OfferReceiver user is: ", offerReceiver.connectionId);
+      socket.to(offerReceiver.connectionId).emit("ReceiveOffer", data);
+    }
+  });
+  socket.on("answerSentToUser1", (data) => {
+    var answerReceiver = userConnection.find(
+      (o) => o.user_id === data.receiver
+    );
+    if (answerReceiver) {
+      console.log("answerReceiver user is: ", answerReceiver.connectionId);
+      socket.to(answerReceiver.connectionId).emit("ReceiveAnswer", data);
+    }
+  });
+  socket.on("candidateSentToUser", (data) => {
+    var candidateReceiver = userConnection.find(
+      (o) => o.user_id === data.remoteUser
+    );
+    if (candidateReceiver) {
+      console.log(
+        "candidateReceiver user is: ",
+        candidateReceiver.connectionId
+      );
+      socket.to(candidateReceiver.connectionId).emit("candidateReceiver", data);
+    }
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected");
+    var disUser = userConnection.find((p) => (p.connectionId = socket.id));
+    if (disUser) {
+      userConnection = userConnection.filter(
+        (p) => (p.connectionId = !socket.id)
+      );
+      console.log(
+        "Rest users username are: ",
+        userConnection.map(function (user) {
+          return user.user_id;
+        })
+      );
+    }
+  });
+});
