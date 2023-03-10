ae4a5276780400d1204fa249b9d159ba9bc5e20b
Borguuh <abdrahmanadewale@gmail.com>
2023-03-10 09:44:27 +0100

    mongoose warning fixes

--- Server/database/connection.js
+++ Server/database/connection.js
@@ -3,7 +3,12 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     //mongodb connection string
-    const con = await mongoose.connect(process.env.MONGO_URI);
+    const con = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useFindAndModify: false,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    });
     console.log(`MongoDB connected: ${con.connection.host}`);
   } catch (err) {
     console.log(err);
